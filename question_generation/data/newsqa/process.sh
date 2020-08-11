a4480b5997d89ead8d587f82656df52b00da19f3
willywsm <willywsm1013@gmail.com>
2020-08-11 13:20:53 +0800

    newsqa process

--- question_generation/data/newsqa/process.sh
+++ question_generation/data/newsqa/process.sh
@@ -1,8 +1,7 @@
-# download NewsQA from https://github.com/Maluuba/newsqa
-# run python maluuba/newsqa/data_generator.py using py2
+# follow the READMD.md in https://github.com/Maluuba/newsqa to download NewsQA data
 
 # convert to squad format
 python convert_to_squad.py
 
-# create short context NewsQA
+# extract context in train.json and truncate 
 python truncate.py train.json 2
